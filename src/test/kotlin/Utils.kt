fc7a8d88fd38e721fc0cb1a03638e7040771190a
Jesper Hustad <jesper.forrest.hustad@nav.no>
2026-01-16 11:59:38 +0100

    Legg til accesspolicy service discovery

--- src/test/kotlin/Utils.kt
+++ src/test/kotlin/Utils.kt
@@ -4,6 +4,7 @@ import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.readBytes
+import io.ktor.client.statement.readRawBytes
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
@@ -58,5 +59,5 @@ fun hentPdf(
             throw RuntimeException("Expected HTTP 200 OK but got ${response.status}")
         }
 
-        response.readBytes()
+        response.readRawBytes()
     }
