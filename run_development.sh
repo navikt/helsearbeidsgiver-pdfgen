4807aba7e415e9d24ff237674f284c90f0a0bc49
Jesper Hustad <jesper.forrest.hustad@nav.no>
2026-02-24 11:13:25 +0100

    Forbedring til run development script

--- run_development.sh
+++ run_development.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+LOCAL_PORT="8080"
 CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+VERSION=$(head -n 1 "$(dirname "$0")/Dockerfile" | cut -d':' -f2) # Henter versjon av pdfgen automatisk fra Dockerfile
+echo -e "\033[1;33m[INFO] Hentet pdfgen versjon $VERSION automatisk fra Dockerfile\033[0m"
 
-docker pull ghcr.io/navikt/pdfgen:2.0.101
+docker pull ghcr.io/navikt/pdfgen:$VERSION
+
+(sleep 3; \
+        printf "\r\n"; \
+        printf "\r%s\n" "PDFGEN server starter i DEV modus"; \
+        printf "\r%s\n" "Endringer i templates, fonts, data eller resources vil automatisk oppdateres i serveren"; \
+        printf "\r%s\n" "http://0.0.0.0:$LOCAL_PORT/api/v1/genpdf/sykmelding/sykmelding"; \
+        printf "\r%s\n" "http://0.0.0.0:$LOCAL_PORT/api/v1/genpdf/soknad/soknad") &
+
 docker run \
         -v $CURRENT_PATH/templates:/app/templates \
         -v $CURRENT_PATH/fonts:/app/fonts \
         -v $CURRENT_PATH/data:/app/data \
         -v $CURRENT_PATH/resources:/app/resources \
-        -p 8080:8080 \
+        -p $LOCAL_PORT:8080 \
         -e DISABLE_PDF_GET=false \
+        -e DEV_MODE=true \
         -e JDK_JAVA_OPTIONS \
         -it \
         --rm \
-        ghcr.io/navikt/pdfgen:2.0.101
+        ghcr.io/navikt/pdfgen:$VERSION
+
